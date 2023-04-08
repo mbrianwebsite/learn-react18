1fe244a5fc33e708341030b81594ff1f366da135
MBrian Website <muhammad.brian.27@gmail.com>
2023-04-08 07:28:43 +0700

    LEARN STATE WITH COUNTER

--- public/app.js
+++ public/app.js
@@ -1,5 +1,16 @@
 const root = ReactDOM.createRoot(document.getElementById("root"));
 function App() {
-  return /*#__PURE__*/React.createElement("h1", null, "Halo");
+  const state = React.useState(0);
+  const count = state[0];
+  const setCount = state[1];
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("button", {
+    onClick: function () {
+      setCount(count - 1);
+    }
+  }, "-"), /*#__PURE__*/React.createElement("span", null, count), /*#__PURE__*/React.createElement("button", {
+    onClick: function () {
+      setCount(count + 1);
+    }
+  }, "-"));
 }
 root.render( /*#__PURE__*/React.createElement(App, null));
--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
 const root = ReactDOM.createRoot(document.getElementById("root"));
 function App() {
-  return <h1>Halo</h1>;
+  const state = React.useState(0);
+
+  const count = state[0];
+  const setCount = state[1];
+  return (
+    <>
+      <button
+        onClick={function () {
+          setCount(count - 1);
+        }}
+      >
+        -
+      </button>
+      <span>{count}</span>
+      <button
+        onClick={function () {
+          setCount(count + 1);
+        }}
+      >
+        -
+      </button>
+    </>
+  );
 }
 root.render(<App />);
